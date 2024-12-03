092eaf7379d8ed959ef87aa73d8ca3925f0d456a
Brice Metthey <b.metthey@gmail.com>
2024-12-03 14:46:11 +0100

    Add files via upload

---
+++ Sequence_2_représentation_des_données/Représentation_image/drapeau_danois.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+
+# Les dimensions de l'image
+largeur = 7
+hauteur = 5
+
+# Création d'une image en mode RGB de 7x5 en blanc
+img = Image.new("RGB", (largeur, hauteur) , (255,255,255) )
+
+
+# On saute de ligne en ligne PUIS on change de colonne
+for x in range (largeur) : # boucle sur les colonnes de l’image
+    
+    for y in range (hauteur): # boucle sur les lignes de l’image
+        
+        print(f"Trace - colonne:{x} ligne:{y}")
+        
+        img.putpixel ((x,y),(255,0,0))
+        
+
+
+for x in range (largeur) : 
+    
+    for y in range (hauteur): 
+        
+        if x == 2 :
+            img.putpixel ((x,y),(255,255,255))
+        if y == 2 :
+            img.putpixel ((x,y),(255,255,255))
+        
+        
+img.show() # affiche l’image
